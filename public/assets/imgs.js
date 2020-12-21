0807f78903a469bb798da6c83c57cdd8bc0c2d15
Daniel Barriga <daniel.barriga.grados@gmail.com>
2020-12-21 22:09:24 +0100

    eliminado datos fantasma

--- public/assets/imgs.js
+++
@@ -1,9 +0,0 @@
-import Iphone7 from "./img/Iphone_7.png";
-import Xiaomi10 from "./img/Xiaomi10_.png";
-
-export default [
-  {
-    "IPhone_7.png": Iphone7,
-    "Xiaomi10_.png": Xiaomi10,
-  },
-];
