60e35890969b06008e647a4a60c0a3688f69dab7
Daniel Barriga <daniel.barriga.grados@gmail.com>
2020-12-26 01:12:47 +0100

    complete app

--- src/components/phoneGrid.js
+++ src/components/phoneGrid.js
@@ -5,11 +5,11 @@ import { UseFetchPhones } from "../hooks/useFetchPhones";
 
 // Bootstrap Components
 import Jumbotron from "react-bootstrap/Jumbotron";
-import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
 
 // Icons
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMobileAlt } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faMobileAlt } from "@fortawesome/free-solid-svg-icons";
 
 // Customs Compoenents
 import PhoneItem from "./phoneItem";
@@ -22,13 +22,12 @@ const PhoneGrid = () => {
     <>
       <Jumbotron className="mb-0">
         <h1>Phone List</h1>
-        <p></p>
         <p>
-          <Button variant="primary">
+          {/* <Button variant="primary">
             <span>
               Add phone <FontAwesomeIcon className="ml-2" icon={faMobileAlt} />
             </span>
-          </Button>
+          </Button> */}
         </p>
       </Jumbotron>
       <div className="container">
@@ -36,7 +35,6 @@ const PhoneGrid = () => {
 
         <div className="grid-row">
           {data.map((data) => {
-            console.log(data);
             return (
               <div key={data._id} className="animate__animated animate__fadeIn">
                 <PhoneItem key={data._id} phone={data}></PhoneItem>
